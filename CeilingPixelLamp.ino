fc95fe8490ff11f4a55bba8f1b9c38cfff54abad
elektrospy <elektrospy@gmail.com>
2016-04-04 21:49:36 +0200

    small code formatting and timing changes added new min/max trigger distance, based on ceiling height

--- CeilingPixelLamp.ino
+++ CeilingPixelLamp.ino
@@ -22,10 +22,10 @@
 
 #if defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny45__)
 //Code in here will only be compiled if an Arduino Tiny45/85 is used.
-#define LEDRGBPIN 1 // WS2812b, , PWM (LED on Model A)
-#define LEDWHITEPIN 4 // white led, PWM, Analog (also used for USB- when USB is in use)
-#define ECHOPIN 3 // ultrasonic echo, Analog In (also used for USB+ when USB is in use)
-#define TRIGPIN 5 // ultrasonic trigger, Analog in
+#define LEDRGBPIN 3 // WS2812b, , PWM (LED on Model A)
+#define LEDWHITEPIN 5 // white led, PWM, Analog (also used for USB- when USB is in use)
+#define ECHOPIN 1 // ultrasonic echo, Analog In (also used for USB+ when USB is in use)
+#define TRIGPIN 4 // ultrasonic trigger, Analog in
 #define I2CSDAPIN 0 // I2C SDA, PWM (LED on Model B)
 #define I2CSCLPIN 2 // I2C SCL, Analog in
 #endif
@@ -47,21 +47,21 @@ const uint8_t autoCycle = 0; // color cycle auto switch
 uint8_t autoCycleDirection = 1; // current direction of auto color cycle
 
 // ultra sonic sensor
-const uint8_t minimumRange = 0; // Minimum range needed in cm
-const uint8_t maximumRange = 50; // Maximum range needed in cm
+const uint8_t minimumRange = 60; // Minimum range needed in cm
+const uint8_t maximumRange = 120; // Maximum range needed in cm
 uint32_t duration = 0; // ultra sonic sensor echo duration
 uint32_t distance = 0; // ultra sonic sensor distance
 //Metro sonicMetro = Metro(50); // refresh time in ms for sensor
-uint32_t sonicPrevMillis = 0;
-const uint32_t sonicInterval = 100;
+uint32_t sonicPrevMillis = 0; // previous ultrasonic sensor polling millis
+const uint32_t sonicInterval = 100; // ultrasonic sensor polling interval
 
 // program logic related variables
 uint8_t getNewColor = 0; // trigger flag for new color generation based on distance
 uint8_t FadingIn = 0; // trigger flag for automatic fade in
 uint8_t FadingOut = 0; // trigger flag for automatic fade out
-uint32_t fadePrevMillis = 0;
-const uint32_t fadeInterval = 25; // fade in and out refresh interval (ms till next increase)
-uint32_t sonicLastPrevMillis = 0;
+uint32_t fadePrevMillis = 0; // previous fade millis
+const uint32_t fadeInterval = 20; // fade in and out refresh interval (ms till next increase)
+uint32_t sonicLastPrevMillis = 0; // previous ultrasonic sensor inactive millis
 const uint32_t sonicLastInterval = 1000; // ultrasonic sensor inactive interval
 
 
@@ -78,40 +78,12 @@ void setup() {
 	//LED.setColorOrderRGB();  // RGB color order
 	//LED.setColorOrderBRG();  // BRG color order
 	LED.setColorOrderGRB();  // GRB color order (Default; will be used if none other is defined.)
+
+	if(autoCycle == 1)
+		brightness = brightnessMax;
 }
 
 void loop() {
-	uint32_t currentMillis = millis(); // get current millis // get current millis
-
-	if(FadingIn == 1) { // still fading in?
-		if (brightness < brightnessMax) { // current brightness still under max brightness ?
-			currentMillis = millis(); // get current millis
-  			if (currentMillis - fadePrevMillis > fadeInterval) { // is it time to fade ?
-				++brightness; // increase brightness by one
-				fadePrevMillis = currentMillis; // save the last time the hue faded one step
-			}
-		}
-		else if(brightness == brightnessMax) { // current brightness reached max brightness ?
-			FadingIn = 0; // stop fading in
-		}
-	}
-	else if(FadingOut == 1) { // still fading out?
-		if (brightness > brightnessMin) { // current brightness still over min brightness?
-			currentMillis = millis(); // get current millis
-			if (currentMillis - fadePrevMillis > fadeInterval) { // is it time to fade?
-				--brightness; // decrease brightness by one
-				fadePrevMillis = currentMillis; // save the last time the hue faded one step
-			}
-		}
-		else if(brightness == brightnessMin) { // current brightness reached min brightness ?
-			FadingOut = 0; // stop fading out
-		}
-	}
-	else if(FadingOut == 1 && FadingIn == 1) { // something got wrong, you need to chose one
-		FadingIn = 0; // stop fading in
-		FadingOut = 0; // stop fading out
-	}
-	
 	if(autoCycle == 1) { // cycle through hue color wheel?
 		if (autoCycleDirection == 1) { // cycle forward ...
 			++hue; // increase hue by one
@@ -126,27 +98,46 @@ void loop() {
 			}
 		}
 	}
+	else if(FadingIn == 1) { // still fading in?
+		if (brightness < brightnessMax) { // current brightness still under max brightness ?
+  			if (millis() - fadePrevMillis > fadeInterval) { // is it time to fade ?
+				fadePrevMillis = millis(); // save the last time the hue faded one step
+				++brightness; // increase brightness by one
+			}
+		}
+		else if(brightness == brightnessMax) { // current brightness reached max brightness ?
+			FadingIn = 0; // stop fading in
+		}
+	}
+	else if(FadingOut == 1) { // still fading out?
+		if (brightness > brightnessMin) { // current brightness still over min brightness?
+			if (millis() - fadePrevMillis > fadeInterval) { // is it time to fade?
+				fadePrevMillis = millis(); // save the last time the hue faded one step
+				--brightness; // decrease brightness by one
+			}
+		}
+		else if(brightness == brightnessMin) { // current brightness reached min brightness ?
+			FadingOut = 0; // stop fading out
+		}
+	}
 	else {
-		currentMillis = millis(); // get current millis
 		// check if ultra-sonic-sensor refresh interval breached
-		if (currentMillis - sonicPrevMillis > sonicInterval) {
+		if (millis() - sonicPrevMillis > sonicInterval) {
+			sonicPrevMillis = millis(); // save current ultrasonic sensor check time
 			refreshRawDistance(); // get current distance from ultrasonic sensor
 			// "inside of range" logic
 			if (distance > minimumRange && distance < maximumRange) {
-				currentMillis = millis(); // get current millis
+				sonicLastPrevMillis = millis(); // save millis for distance inside limit
 				if(getNewColor == 1) {
 					// map distance 
 					hue = map(distance, minimumRange, maximumRange, 0, hueRange);
 					getNewColor = 0; // queue new color request
 					FadingIn = 1; // queue fade in effect
 				}
-				sonicLastPrevMillis = currentMillis;
 			}
-			sonicPrevMillis = currentMillis; // save current ultrasonic sensor check time
-		} // end if (currentMillis - sonicPrevMillis > sonicInterval) 
-		currentMillis = millis(); // get current millis
+		} // end if (currentMillis - sonicPrevMillis > sonicInterval)
 		// check last ultrasonic sensor event interval
-		if (currentMillis - sonicLastPrevMillis > sonicLastInterval) {
+		if (millis() - sonicLastPrevMillis > sonicLastInterval) {
 			getNewColor = 1; // queue new color request
 			FadingOut = 1; // queue fade out effect
 		}
