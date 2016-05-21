914c4a4df7a4b3c03db2bfe284934364ac19a602
elektrospy <elektrospy@gmail.com>
2016-05-21 22:12:32 +0200

    small changes to test the hardware prototype 2

--- CeilingPixelLamp.ino
+++ CeilingPixelLamp.ino
@@ -1,4 +1,5 @@
 #include <WS2812.h>
+#include <NewPing.h>
 
 #if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
 //Code in here will only be compiled if an Arduino Uno (or older) is used.
@@ -37,32 +38,46 @@ cRGB ledValue; // holds the RGB color values
 const uint8_t saturation = 255; 	// color saturation
 uint8_t brightness = 0; 			// start / current brightness
 const uint8_t brightnessMin = 0;	// lowest limit of led brightness
-const uint8_t brightnessMax = 32;	// highest limit of led brightness
+const uint8_t brightnessMax = 255;	// highest limit of led brightness
 uint8_t rgbColor[3] = {0,0,0}; 		// inital RGB values buffer array
-const uint16_t hueRange = 768;		// hue color range (3x255 from RGB)
+const uint16_t hueRange = 765;		// hue color range (3x255 from RGB)
 uint16_t hue = 0;					// current Hue color
 
 // auto cycle color / rainbow wheel
 const uint8_t autoCycle = 0; // color cycle auto switch
 uint8_t autoCycleDirection = 1; // current direction of auto color cycle
 
+// white led stuff
+const uint8_t autoWhite = 1;
+uint8_t WhiteFadingIn = 0;
+uint8_t WhiteFadingOut = 1;
+uint32_t WhitefadePrevMillis = 0; // previous fade millis
+const uint32_t WhiteFadeInterval = 5; // fade in and out refresh interval (ms till next increase)
+uint8_t Whitebrightness = 0;
+uint8_t WhiteBrightnessMin = 0;
+uint8_t WhiteBrightnessMax = 255;
+
 // ultra sonic sensor
 const uint8_t minimumRange = 60; // Minimum range needed in cm
 const uint8_t maximumRange = 120; // Maximum range needed in cm
-uint32_t duration = 0; // ultra sonic sensor echo duration
+//const uint8_t minimumRange = 10; // Minimum range needed in cm
+//const uint8_t maximumRange = 80; // Maximum range needed in cm
+
 uint32_t distance = 0; // ultra sonic sensor distance
 //Metro sonicMetro = Metro(50); // refresh time in ms for sensor
 uint32_t sonicPrevMillis = 0; // previous ultrasonic sensor polling millis
-const uint32_t sonicInterval = 100; // ultrasonic sensor polling interval
+const uint32_t sonicInterval = 40; // ultrasonic sensor polling interval
+uint32_t sonicLastPrevMillis = 0; // previous ultrasonic sensor inactive millis
+const uint32_t sonicLastInterval = 500; // ultrasonic sensor inactive interval
+NewPing sonar(ECHOPIN, ECHOPIN, maximumRange); // NewPing setup of pin and maximum distance.
 
 // program logic related variables
 uint8_t getNewColor = 0; // trigger flag for new color generation based on distance
 uint8_t FadingIn = 0; // trigger flag for automatic fade in
 uint8_t FadingOut = 0; // trigger flag for automatic fade out
 uint32_t fadePrevMillis = 0; // previous fade millis
-const uint32_t fadeInterval = 20; // fade in and out refresh interval (ms till next increase)
-uint32_t sonicLastPrevMillis = 0; // previous ultrasonic sensor inactive millis
-const uint32_t sonicLastInterval = 1000; // ultrasonic sensor inactive interval
+const uint32_t fadeInterval = 5; // fade in and out refresh interval (ms till next increase)
+
 
 
 void refreshRawDistance(); // get raw distance from ultrasonic sensor
@@ -70,8 +85,8 @@ void hsb2rgb(uint16_t index, uint8_t sat, uint8_t bright, uint8_t color[3]); // 
 
 void setup() {
 	// supersonic sensor pin setup
-	pinMode(ECHOPIN, INPUT); // set ultrasonic sensor echo pin to input
-	pinMode(TRIGPIN, OUTPUT); // set ultrasonic sensor trigger pin to output
+	//pinMode(ECHOPIN, INPUT); // set ultrasonic sensor echo pin to input
+	//pinMode(TRIGPIN, OUTPUT); // set ultrasonic sensor trigger pin to output
 
 	// LED pin and color order setup
 	LED.setOutput(LEDRGBPIN); // set WS2812B pin for library
@@ -79,11 +94,47 @@ void setup() {
 	//LED.setColorOrderBRG();  // BRG color order
 	LED.setColorOrderGRB();  // GRB color order (Default; will be used if none other is defined.)
 
-	if(autoCycle == 1)
+	if(autoCycle == 1) {
+		//brightness = 0;
 		brightness = brightnessMax;
+	}
 }
 
 void loop() {
+	if(autoWhite == 1){
+		if(WhiteFadingIn == 1) { // still fading in?
+			if (Whitebrightness < WhiteBrightnessMax) { // current Whitebrightness still under max Whitebrightness ?
+	  			if (millis() - WhitefadePrevMillis > WhiteFadeInterval) { // is it time to fade ?
+					WhitefadePrevMillis = millis(); // save the last time the hue faded one step
+					++Whitebrightness; // increase Whitebrightness by one
+				}
+			}
+			else if(Whitebrightness == WhiteBrightnessMax) { // current Whitebrightness reached max Whitebrightness ?
+				WhiteFadingIn = 0; // stop fading in
+			}
+		}
+		else if(WhiteFadingOut == 1) { // still fading out?
+			if (Whitebrightness > WhiteBrightnessMin) { // current Whitebrightness still over min Whitebrightness?
+				if (millis() - WhitefadePrevMillis > WhiteFadeInterval) { // is it time to fade?
+					WhitefadePrevMillis = millis(); // save the last time the hue faded one step
+					--Whitebrightness; // decrease Whitebrightness by one
+				}
+			}
+			else if(Whitebrightness == WhiteBrightnessMin) { // current Whitebrightness reached min Whitebrightness ?
+				WhiteFadingOut = 0; // stop fading out
+			}
+		}
+		if(WhiteFadingIn == 1 || WhiteFadingOut == 1) {
+			analogWrite(LEDWHITEPIN, Whitebrightness);
+		}
+		/*
+		else {
+			analogWrite(LEDWHITEPIN, 0);
+		}
+		*/
+	}
+
+
 	if(autoCycle == 1) { // cycle through hue color wheel?
 		if (autoCycleDirection == 1) { // cycle forward ...
 			++hue; // increase hue by one
@@ -97,6 +148,7 @@ void loop() {
 				autoCycleDirection = 1; // change direction to forward
 			}
 		}
+		delay(20);
 	}
 	else if(FadingIn == 1) { // still fading in?
 		if (brightness < brightnessMax) { // current brightness still under max brightness ?
@@ -133,6 +185,7 @@ void loop() {
 					hue = map(distance, minimumRange, maximumRange, 0, hueRange);
 					getNewColor = 0; // queue new color request
 					FadingIn = 1; // queue fade in effect
+					WhiteFadingOut = 1;
 				}
 			}
 		} // end if (currentMillis - sonicPrevMillis > sonicInterval)
@@ -140,6 +193,7 @@ void loop() {
 		if (millis() - sonicLastPrevMillis > sonicLastInterval) {
 			getNewColor = 1; // queue new color request
 			FadingOut = 1; // queue fade out effect
+			WhiteFadingIn = 1;
 		}
 	}
 
@@ -156,18 +210,9 @@ void loop() {
 	LED.sync(); // Sends the data to the LED strip
 }
 
-void refreshRawDistance(){
-	/* The following TRIGPIN/ECHOPIN cycle is used to determine the
-		distance of the nearest object by bouncing soundwaves off of it. */ 
-	digitalWrite(TRIGPIN, LOW); 
-	delayMicroseconds(2);
-	digitalWrite(TRIGPIN, HIGH);
-	delayMicroseconds(10);
-	digitalWrite(TRIGPIN, LOW);
-	duration = pulseIn(ECHOPIN, HIGH); // wait for echo wave and return the duration
-	// Calculate the distance (in centimeter or inches) based on the speed of sound.
-	//distance = duration / 74 / 2; // calculate inches
-	distance = duration / 29 / 2; // calculate centimeter
+void refreshRawDistance() {
+	unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
+	distance = uS / US_ROUNDTRIP_CM; // Convert ping time to distance (0 = outside set distance range, no ping echo)
 }
 
 void hsb2rgb(uint16_t index, uint8_t sat, uint8_t bright, uint8_t color[3]) {
